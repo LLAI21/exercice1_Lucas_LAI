e26eb0df21f59a867071ee79aa7880625a3983d2
LLAI21 <lucas.travail21@gmail.com>
2025-10-15 09:39:59 +0200

    J2

--- exercice1_Lucas_LAI/model/task.py
+++ exercice1_Lucas_LAI/model/task.py
@@ -1,15 +1,15 @@
 class Task:
     # """Représente une tâche de la ToDoList."""
 
-    def __init__(self, title, done=False):
+    def __init__(self, title):
         self.title = title
-        self.done = done
+        # self.done = done
 
     # def mark_done(self):
     #     # """Marque la tâche comme terminée."""
     #     self.done = True
 
-    # def __str__(self):
-    #     # """Affichage lisible de la tâche."""
-    #     status = "✅" if self.done else "❌"
-    #     return f"{self.title} {status}"
+    def __str__(self):
+        # """Affichage lisible de la tâche."""
+        # status = "✅" if self.title else "❌"
+        return f"{self.title}"
